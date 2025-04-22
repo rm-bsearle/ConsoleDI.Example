002bc015416f7c47ef3811548e68672ecea31245
Searle, Brian <BrianSearle@rocketmortgage.com>
2025-04-22 12:02:30 -0400

    Add a service that requires DI

---
+++ ConsoleDI.Example.Console/ServiceLifetimeReporter.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleDI.Example.Console;
+
+public class ServiceLifetimeReporter(
+    IExampleTransientService transientService,
+    IExampleScopedService scopedService,
+    IExampleSingletonService singletonService)
+{
+    public void ReportServiceLifetimeDetails(string lifetimeDetails)
+    {
+        System.Console.WriteLine(lifetimeDetails);
+
+        LogService(transientService, "Always different");
+        LogService(scopedService, "Changes only with lifetime");
+        LogService(singletonService, "Always the same");
+    }
+
+    private static void LogService<T>(T service, string message)
+        where T : IReportServiceLifetime =>
+        System.Console.WriteLine(
+            $"    {typeof(T).Name}: {service.Id} ({message})");
+}
