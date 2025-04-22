0713a63f0816999f56bca930d8e788b70a4e7a3f
Searle, Brian <BrianSearle@rocketmortgage.com>
2025-04-22 11:51:27 -0400

    Add default implementations

---
+++ ConsoleDI.Example.Console/ExampleSingletonService.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleDI.Example.Console;
+
+internal sealed class ExampleSingletonService : IExampleSingletonService
+{
+    Guid IReportServiceLifetime.Id { get; } = Guid.NewGuid();
+}
---
+++ ConsoleDI.Example.Console/ExampleTransientService.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsoleDI.Example.Console;
+
+internal sealed class ExampleTransientService : IExampleTransientService
+{
+    Guid IReportServiceLifetime.Id { get; } = Guid.NewGuid();
+}
